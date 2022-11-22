b1211bce3dd5a8ad68bc11814750e9479bc7ba09
haomingjue <haomingjue@outlook.com>
2022-11-22 12:14:37 -0500

    finished services

--- photosharing/src/main/java/com/photosharingwebsite/photosharing/service/PhotoService.java
+++ photosharing/src/main/java/com/photosharingwebsite/photosharing/service/PhotoService.java
@@ -4,11 +4,7 @@ import com.photosharingwebsite.photosharing.model.Photo;
 import com.photosharingwebsite.photosharing.model.User;
 import com.photosharingwebsite.photosharing.repository.PhotoRepository;
 import com.photosharingwebsite.photosharing.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -20,7 +16,6 @@ public class PhotoService {
 
     private final PhotoRepository photoRepository;
 
-    private MongoTemplate mongoTemplate;
 
     public PhotoService(UserRepository userRepository, PhotoRepository photoRepository) {
         this.userRepository = userRepository;
@@ -61,7 +56,6 @@ public class PhotoService {
 
     public void unLikePhoto(String username, String photoTitle) {
         try {
-//            Query query = new Query().addCriteria(Criteria.where("username").is(username));
             User user = userRepository.findByUsername(username).get(0);
             HashSet<String> currentLikedPhotos = user.getLikedPhotos();
             currentLikedPhotos.remove(photoTitle);
