51beba053a18e850ae548e1ad3cc22121593c59a
haomingjue <haomingjue@outlook.com>
2022-12-04 20:55:33 -0500

    updated cors config

---
+++ src/main/java/com/photosharingwebsite/photosharing/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.photosharingwebsite.photosharing.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
